07465d665da28454ba9951a19fefb5e3a5998005
Mahmud Ahsan <mahmud@thinkdiff.net>
2019-07-04 14:22:59 +0800

    code refactored add stateful widget in the home screen

--- lib/main.dart
+++ lib/main.dart
@@ -17,66 +17,83 @@ class TodosApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: kAppTitle,
-      home: Scaffold(
-        backgroundColor: Color(0xfffff5eb),
-        body: SafeArea(
-          child: CustomScrollView(
-            slivers: <Widget>[
-              SliverAppBar(
-                backgroundColor: Color(0x00),
-                floating: true,
-                flexibleSpace: Container(
-                    child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Header(),
-                    Container(
-                      margin: EdgeInsets.only(right: 10, top: 35),
-                      width: 30,
-                      child: GestureDetector(
-                        onTap: () {
-                          print('More Settings Clicked');
-                        },
-                        child: Icon(
-                          Icons.more_vert,
-                          size: 30,
-                        ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String welcomeMsg = 'Good Morning';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xfffff5eb),
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              backgroundColor: Color(0x00),
+              floating: true,
+              flexibleSpace: Container(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Header(
+                    msg: welcomeMsg,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 10, top: 35),
+                    width: 30,
+                    child: GestureDetector(
+                      onTap: () {
+                        print('More Settings Clicked');
+                      },
+                      child: Icon(
+                        Icons.more_vert,
+                        size: 30,
                       ),
                     ),
-                  ],
-                )),
-                expandedHeight: 100,
+                  ),
+                ],
+              )),
+              expandedHeight: 100,
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  switch (index) {
+                    case 0:
+                      return Todo();
+                    case 1:
+                      return SizedBox(
+                        height: 50,
+                      );
+                    default:
+                      return Done();
+                  }
+                },
+                childCount: 3,
               ),
-              SliverList(
-                delegate: SliverChildBuilderDelegate(
-                  (context, index) {
-                    switch (index) {
-                      case 0:
-                        return Todo();
-                      case 1:
-                        return SizedBox(
-                          height: 50,
-                        );
-                      default:
-                        return Done();
-                    }
-                  },
-                  childCount: 3,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print('Add Task');
-          },
-          child: Icon(
-            Icons.add,
-            color: Colors.white,
-          ),
-          backgroundColor: Color(0xffffca3e47),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {});
+          print('Add Task');
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
         ),
+        backgroundColor: Color(0xffffca3e47),
       ),
     );
   }
--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -5,13 +5,17 @@
 import 'package:flutter/material.dart';
 
 class Header extends StatelessWidget {
+  final String msg;
+
+  Header({this.msg});
+
   @override
   Widget build(BuildContext context) {
     return LimitedBox(
       child: Container(
         padding: EdgeInsets.only(left: 15.0, top: 30.0),
         child: Text(
-          'Good Morning',
+          msg,
           style: Theme.of(context)
               .textTheme
               .headline
