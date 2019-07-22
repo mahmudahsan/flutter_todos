d57bd17f1cae7894cfa94f28a36a58eca5f66abe
Mahmud Ahsan <mahmud@thinkdiff.net>
2019-07-22 13:35:38 +0800

    code refactored by oop to simple functions for firebase authentication. also social icons buttons added via 3rd party plugins

--- lib/service/firebase_auth.dart
+++ lib/service/firebase_auth.dart
@@ -8,57 +8,72 @@ import 'package:google_sign_in/google_sign_in.dart';
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn _googleSignIn = GoogleSignIn();
 
-class FirebaseAuthentication {
-  static final FirebaseAuthentication instance = FirebaseAuthentication._();
+Future<bool> isUserSignedIn() async {
+  final FirebaseUser currentUser = await _auth.currentUser();
+  return currentUser != null;
+}
 
-  FirebaseAuthentication._();
-
-  Future<bool> isUserSignedIn() async {
-    final FirebaseUser currentUser = await _auth.currentUser();
-    return currentUser != null;
-  }
-
-  void signOut() {
-    try {
-      _auth.signOut();
-    } catch (error) {
-      print(error);
-    }
-  }
-
-  void onAuthenticationChange(Function isLogin) {
-    _auth.onAuthStateChanged.listen((FirebaseUser user) {
-      if (user != null) {
-        isLogin(true);
-      } else {
-        isLogin(false);
-      }
-    });
-  }
-
-  Future<Map<String, String>> signInWithGoogle() async {
-    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-    final GoogleSignInAuthentication googleAuth =
-        await googleUser.authentication;
-
-    final AuthCredential credential = GoogleAuthProvider.getCredential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-
-    final FirebaseUser user = await _auth.signInWithCredential(credential);
-
-    assert(user.email != null);
-    assert(user.displayName != null);
-    assert(!user.isAnonymous);
-    assert(await user.getIdToken() != null);
-
-    final FirebaseUser currentUser = await _auth.currentUser();
-    assert(user.uid == currentUser.uid);
-
-    if (user != null) {
-      return {'username': user.displayName, 'uid': user.uid};
-    }
-    return null;
+void signOut() {
+  try {
+    _auth.signOut();
+  } catch (error) {
+    print(error);
   }
 }
+
+void onAuthenticationChange(Function isLogin) {
+  _auth.onAuthStateChanged.listen((FirebaseUser user) {
+    if (user != null) {
+      isLogin(true);
+    } else {
+      isLogin(false);
+    }
+  });
+}
+
+Future<Map<String, String>> signInWithGoogle() async {
+  final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+  final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+  final AuthCredential credential = GoogleAuthProvider.getCredential(
+    accessToken: googleAuth.accessToken,
+    idToken: googleAuth.idToken,
+  );
+
+  final FirebaseUser user = await _auth.signInWithCredential(credential);
+
+  assert(user.email != null);
+  assert(user.displayName != null);
+  assert(!user.isAnonymous);
+  assert(await user.getIdToken() != null);
+
+  final FirebaseUser currentUser = await _auth.currentUser();
+  assert(user.uid == currentUser.uid);
+
+  if (user != null) {
+    return {'username': user.displayName, 'uid': user.uid};
+  }
+  return null;
+}
+
+//Future<Map<String, String>> signInWithFacebook() async {
+//  final AuthCredential credential = FacebookAuthProvider.getCredential(
+//    accessToken: _tokenController.text,
+//  );
+//
+//  final FirebaseUser user = await _auth.signInWithCredential(credential);
+//  assert(user.email != null);
+//  assert(user.displayName != null);
+//  assert(!user.isAnonymous);
+//  assert(await user.getIdToken() != null);
+//
+//  final FirebaseUser currentUser = await _auth.currentUser();
+//  assert(user.uid == currentUser.uid);
+//  setState(() {
+//    if (user != null) {
+//      _message = 'Successfully signed in with Facebook. ' + user.uid;
+//    } else {
+//      _message = 'Failed to sign in with Facebook. ';
+//    }
+//  });
+//}
--- lib/widgets/launch_screen.dart
+++ lib/widgets/launch_screen.dart
@@ -5,7 +5,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todos/widgets/header.dart';
 import 'package:flutter_todos/constants.dart';
-import 'package:flutter_todos/service/firebase_auth.dart';
+import 'package:flutter_todos/utils/colors.dart';
+import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+import 'package:flutter_todos/service/firebase_auth.dart' as FirebaseAuth;
 
 class LaunchScreen extends StatefulWidget {
   bool shouldPopOnSkip;
@@ -23,7 +25,7 @@ class _LaunchScreenState extends State<LaunchScreen> {
   void initState() {
     super.initState();
 
-    FirebaseAuthentication.instance.onAuthenticationChange((bool isLogin) {
+    FirebaseAuth.onAuthenticationChange((bool isLogin) {
       if (isLogin) {
         setState(() {
           isUserSignedIn = true;
@@ -47,7 +49,7 @@ class _LaunchScreenState extends State<LaunchScreen> {
           color: Colors.white,
           child: Container(
             width: MediaQuery.of(context).size.width,
-            height: 380,
+            height: 550,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
@@ -65,13 +67,15 @@ class _LaunchScreenState extends State<LaunchScreen> {
                 getLine(),
                 if (isUserSignedIn) getSignedOut(),
                 if (!isUserSignedIn) getSignIn(),
-                SizedBox(
-                  height: 10,
-                ),
-                getLine(),
+                getLine(marginTop: 15, marginBottom: 10),
+                if (!isUserSignedIn) getManualSignIn(),
+                if (!isUserSignedIn) getLine(marginTop: 10),
                 if (isUserSignedIn) getSynchronizedMessage(),
-                if (isUserSignedIn) getSkip('Go Back'),
-                if (!isUserSignedIn) getSkip('Skip'),
+                if (isUserSignedIn)
+                  getSkip(context, 'Go Back',
+                      Color(TodosColor.kSecondaryColorCode)),
+                if (!isUserSignedIn)
+                  getSkip(context, 'Skip', Color(TodosColor.kPrimaryColorCode)),
               ],
             ),
           ),
@@ -80,12 +84,15 @@ class _LaunchScreenState extends State<LaunchScreen> {
     );
   }
 
-  Widget getLine() {
-    return SizedBox(
-      height: 0.5,
-      width: double.infinity,
-      child: Container(
-        color: Colors.grey,
+  Widget getLine({double marginTop = 0, double marginBottom = 0}) {
+    return Container(
+      margin: EdgeInsets.only(top: marginTop, bottom: marginBottom),
+      child: SizedBox(
+        height: 0.5,
+        width: double.infinity,
+        child: Container(
+          color: Colors.grey,
+        ),
       ),
     );
   }
@@ -96,7 +103,7 @@ class _LaunchScreenState extends State<LaunchScreen> {
       margin: EdgeInsets.only(top: 10),
       child: RaisedButton(
         onPressed: () {
-          FirebaseAuthentication.instance.signOut();
+          FirebaseAuth.signOut();
         },
         child: Text('Logout'),
       ),
@@ -115,83 +122,108 @@ class _LaunchScreenState extends State<LaunchScreen> {
 
   Widget getSignIn() {
     return Container(
+      margin: EdgeInsets.only(top: 15, left: 15, right: 15),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Text('Synchronize and backup your data'),
+            padding: const EdgeInsets.symmetric(vertical: 5),
+            child: Text(
+              'Synchronize and backup your data',
+              style: TextStyle(fontSize: 18),
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              RaisedButton(
-                onPressed: () {},
-                child: Text('Facebook'),
-              ),
-              RaisedButton(
-                onPressed: () async {
-                  try {
-                    Map<String, String> user = await FirebaseAuthentication
-                        .instance
-                        .signInWithGoogle();
-                    if (user != null) {
-                      print('User Name: ' + user['username']);
-                      print('User uid: ' + user['uid']);
-                    } else {
-                      print('User not authenticated');
-                    }
-                  } catch (error) {
-                    print(error);
-                  }
+              FacebookSignInButton(
+                onPressed: () {
+                  // call authentication logic
                 },
-                child: Text('Google'),
               ),
-              RaisedButton(
-                onPressed: () {},
-                child: Text('Twitter'),
+              SizedBox(
+                height: 5,
+              ),
+              GoogleSignInButton(
+                onPressed: googleSignIn,
+                darkMode: true, // default: false
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              TwitterSignInButton(
+                onPressed: () => {},
               ),
             ],
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 10),
-            child: ButtonTheme(
-              minWidth: double.infinity,
-              child: Column(
-                children: <Widget>[
-                  RaisedButton(
-                    onPressed: () {},
-                    child: Text('Create Account'),
-                  ),
-                  RaisedButton(
-                    onPressed: () {},
-                    child: Text('Sign In'),
-                  ),
-                ],
-              ),
-            ),
           ),
         ],
       ),
     );
   }
 
-  Widget getSkip(String title) {
+  Widget getManualSignIn() {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      margin: EdgeInsets.only(left: 15, right: 15),
       child: ButtonTheme(
         minWidth: double.infinity,
-        child: RaisedButton(
-          onPressed: () {
-            if (widget.shouldPopOnSkip) {
-              Navigator.pop(context);
-            } else {
-              print('I do not know what to do');
-            }
-          },
-          child: Text(title),
+        child: Column(
+          children: <Widget>[
+            getButton(context, title: 'Create Account', onPress: () {}),
+            getButton(context, title: 'Sign In', onPress: () {}),
+          ],
         ),
       ),
     );
+  }
+
+  Widget getSkip(BuildContext context, String title, Color color) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      child: ButtonTheme(
+        minWidth: double.infinity,
+        child: getButton(context, title: title, color: color, onPress: () {
+          if (widget.shouldPopOnSkip) {
+            Navigator.pop(context);
+          } else {
+            print('I do not know what to do');
+          }
+        }),
+      ),
+    );
+  }
+
+  Widget getButton(BuildContext context,
+      {String title,
+      @required Function onPress,
+      Color color = Colors.blueAccent}) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 5),
+      height: 50,
+      child: RaisedButton(
+        onPressed: onPress,
+        color: color,
+        child: Text(
+          title,
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: Theme.of(context).textTheme.title.fontSize),
+        ),
+      ),
+    );
+  }
+
+  void googleSignIn() async {
+    try {
+      Map<String, String> user = await FirebaseAuth.signInWithGoogle();
+      if (user != null) {
+        print('User Name: ' + user['username']);
+        print('User uid: ' + user['uid']);
+      } else {
+        print('User not authenticated');
+      }
+    } catch (error) {
+      print(error);
+    }
   }
 }
